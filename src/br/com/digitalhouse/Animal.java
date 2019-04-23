bd32488e603b21d8ac8786e2fd40075a1bcada00
unknown <Digital@DH-A8-10.dhbrasil.digitalhouse.com>
2019-04-22 22:10:21 -0300

    Initial commit

---
+++ src/br/com/digitalhouse/Animal.java
@@ -0,0 +1,25 @@
+package br.com.digitalhouse;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Animal {
+    public static void main(String[] args) {
+        List<String> animais = new ArrayList();
+
+        animais.add("gato");
+        animais.add("pato");
+        animais.add("cachorro");;
+
+        
+            System.out.println(animais.get(4));
+
+
+
+
+        System.out.println();
+
+    }
+
+}
---
+++ src/br/com/digitalhouse/Raças.java
@@ -0,0 +1,31 @@
+package br.com.digitalhouse;
+
+public class Raças {
+    private String gato;
+    private String cachorro;
+    private String pato;
+
+    public String getGato() {
+        return gato;
+    }
+
+    public void setGato(String gato) {
+        this.gato = gato;
+    }
+
+    public String getCachorro() {
+        return cachorro;
+    }
+
+    public void setCachorro(String cachorro) {
+        this.cachorro = cachorro;
+    }
+
+    public String getPato() {
+        return pato;
+    }
+
+    public void setPato(String pato) {
+        this.pato = pato;
+    }
+}
